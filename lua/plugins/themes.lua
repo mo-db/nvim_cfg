7cabf140a65d2a265012281952177592a62576be
Moritz Huber <moritz@Moritzs-MacBook-Pro.local>
2025-02-02 12:43:54 +0100

    slight config changes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,37 +6,69 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd.colorscheme('nord')
+        require('onedark').setup { style = 'dark' }
+        vim.cmd.colorscheme('sonokai')
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
+        require('onedark').setup { style = 'light' }
         vim.cmd.colorscheme('onedark')
+        -- vim.cmd.colorscheme('monokai-pro-light')
       end,
       fallback = "dark"
     },
+  },
+  {
+      "zenbones-theme/zenbones.nvim",
+      dependencies = "rktjmp/lush.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+      end
+  },
+  { 'Mofiqul/vscode.nvim', 
+    lazy = false, 
+    priority = 1000,
+    config = function()
+    end,
+  },
+  { 'catppuccin/nvim', 
+    lazy = false, 
+    priority = 1000,
+    config = function()
+    end,
+  },
+  { '2giosangmitom/nightfall.nvim', 
+    lazy = false, 
+    priority = 1000,
+    config = function()
+    end,
   },
   { 'sainnhe/sonokai', 
     lazy = false, 
     priority = 1000,
     config = function()
       -- shusia, espresso, maia, andromeda, atlantis, default
-      vim.g.sonokai_style = 'shusia'
+      vim.g.sonokai_style = 'andromeda'
     end,
   },
   { 'navarasu/onedark.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-        require('onedark').setup { style = 'dark' }
+      require('onedark').setup{}
     end,
+  },
+  {
+    'comfysage/evergarden',
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      transparent_background = false,
+      variant = 'medium', -- 'hard'|'medium'|'soft'
+      overrides = { }, -- add custom overrides
+    }
   },
   { 'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-    end,
-  },
-  { 'gbprod/nord.nvim',
     lazy = false,
     priority = 1000,
     config = function()
@@ -73,5 +105,11 @@ return {
       require('solarized-osaka').setup{ transparent = false, }
     end,
   },
+  -- { 'gbprod/nord.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --   end,
+  -- },
 }
 -- vim: ts=2 sts=2 sw=2 et
