39729b9e76e849004e4632407ed984e73127c537
Moritz Huber <moritz@Moritzs-MacBook-Pro.local>
2025-02-02 12:49:42 +0100

    slight config changes

--- init.lua
+++ init.lua
@@ -83,6 +83,8 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
+  -- automatically check for plugin updates
+  checker = { enabled = false },
 })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
